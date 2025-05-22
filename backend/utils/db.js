d2d987fc169069ca6cc1241b9aa81d14310e2654
RanjeetRaj444 <ranjeetraj44666@gmail.com>
2025-05-22 15:19:28 +0530

    updaed backend mongodb URL

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/authDB";
+const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/authDB";
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(MONGO_URL);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection error:", error);
